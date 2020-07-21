dded7f0a4a7cb73d8baedb95126aa08540080ae7
Will-Hoey <will.hoey@hotmail.co.uk>
2020-07-21 15:59:37 +0100

    Add settings to allow for django-admin migrations

--- settings.py
+++ settings.py
@@ -35,7 +35,8 @@ TEMPLATES = [
             "context_processors": [
                 "django.contrib.auth.context_processors.auth",
                 "django.template.context_processors.request",
-                "django.contrib.messages.context_processors.messages",            ],
+                "django.contrib.messages.context_processors.messages",
+            ],
         },
     },
 ]
